fa3e280ffbbc6f71fec578436b915f63779d7ea8
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-05-23 08:04:58 +0300

    GameConsole scaffolds

---
+++ spec/codebreaker/game_console_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+module Codebreaker
+  RSpec.describe GameConsole do
+    describe '#new' do
+      it 'initialize object'
+    end
+
+    describe '#start_game' do
+      it 'start game'
+    end
+
+    describe '#submit' do
+      it 'submit guess'
+    end
+
+    describe '#invalid_imput' do
+      it 'wrong input'
+    end
+
+    describe '#end_game' do
+      it 'game over'
+    end
+  end
+end
