34cb314d8eb437b8dfa0b7fb1784e838be39b30a
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-05-24 23:37:07 +0300

    #initialize

--- spec/codebreaker/localization_spec.rb
+++ spec/codebreaker/localization_spec.rb
@@ -2,8 +2,41 @@ require 'spec_helper'
 
 module Codebreaker
   RSpec.describe Localization do
-    describe '#initialize' do
-      it 'initialize object'
+    let(:localization) { Localization.new(:game) }
+
+    describe '#new' do
+      describe '#initialize' do
+        describe 'without args' do
+          specify { expect { subject }.to raise_error(ArgumentError) }
+        end
+
+        describe 'with args' do
+          describe '1 argument' do
+            context 'when wrong app type' do
+              let(:local_wrong_app) { Localization.new(:wrong_type) }
+              specify { expect { local_wrong_app }.to raise_error(RuntimeError, 'Unknown application type.') }
+            end
+            
+            context 'when right app type' do
+              specify { expect(localization).to be_an_instance_of(Localization) }
+            end
+          end
+
+          describe '2 arguments' do
+            context 'when wrong app type' do
+              let(:local_wrong_app) { Localization.new(:wrong_type, :ru) }
+              specify { expect { local_wrong_app }.to raise_error(RuntimeError, 'Unknown application type.') }
+            end
+
+            context 'when wrong lang' do
+              let(:local_wrong_lang) { Localization.new(:console, :eg) }
+              specify { expect(local_wrong_lang).to be_an_instance_of(Localization) }
+            end
+          end
+        end
+      end
+
+      
     end
 
     describe '#localization' do
