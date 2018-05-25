457d71dd073774d92d3ab32385a1302637db0548
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-05-25 21:06:07 +0300

    Implement Console class

--- lib/codebreaker/console.rb
+++ lib/codebreaker/console.rb
@@ -1,7 +1,115 @@
+require 'colorize'
+
 module Codebreaker
   class Console
+    YES = 'y'
+
+    attr_reader :game
+
     def initialize(game)
+      raise ArgumentError, 'Wrong object type!' unless game.is_a?(Game)
       @game = game
+      @game_config_snapshot = game.configuration.clone
+      @locale = Localization.new(:console, game.configuration.lang)
+    end
+
+    def start_game
+      puts message['alerts']['welcome'].colorize(:background => :blue)
+      puts message['alerts']['hint_info']
+      submit_answer
+    end
+
+    private
+
+    def message
+      @locale.localization
+    end
+
+    def submit_answer
+      process(input_script)
+    end
+
+    def show_hint
+      puts begin
+        "#{message['alerts']['hint']}: #{game.hint.to_s.green}"
+      rescue => error
+        error.to_s.red
+      end
+    end
+
+    def input_script
+      unless game.attempts.zero?
+        input, status, step = '', false, 0
+        until status
+          begin
+            game.guess_valid?(input)
+            status = true
+          rescue => error
+            puts error.to_s.red unless step.zero? || input == '-h'
+            puts "#{message['alerts']['guess']}:"
+            input = gets.chomp
+            step += 1
+            show_hint if input == '-h'
+          end
+        end
+        input
+      end
+    end
+
+    def motivation_message
+      if !game.won? && game.attempts == rand(1..game.configuration.max_attempts)
+        message['alerts']['motivation']
+      end
+    end
+
+    def process(input)
+      begin
+        puts game.to_guess(input)
+        puts motivation_message
+      rescue => error
+        puts error.to_s.red
+        finish_game
+      end
+      game.won? ? finish_game : submit_answer
+    end
+
+    def finish_game
+      puts game.won? ? message['alerts']['win'].green : message['alerts']['lose'].red
+      save_game
+      new_game
+    end
+
+    def input_selector
+      input = ''
+        until %w(y n).include?(input)
+          print " (y/n) #{message['alerts']['yes_or_no']}:"
+          input = gets.chomp
+        end
+      input == YES
+    end
+
+    def save_game
+      print message['alerts']['save_game']
+      'save game' if input_selector
+    end
+
+    def new_game
+      print message['alerts']['new_game']
+      if input_selector
+        load_new_game
+        start_game
+      else
+        puts message['alerts']['shutdown']
+        exit
+      end
+    end
+
+    def load_new_game
+      @game = Game.new do |config|
+        @game_config_snapshot.each_pair do |key, value|
+          config[key] = value
+        end
+      end
     end
   end
 end
