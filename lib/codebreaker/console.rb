68b083e03b68f4f885c6cbd09eec9872db0e9212
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-05-29 19:48:48 +0300

    Add #current_user_score

--- lib/codebreaker/console.rb
+++ lib/codebreaker/console.rb
@@ -118,6 +118,16 @@ module Codebreaker
       scores << current_user_score
     end
 
+    def current_user_score
+      Score.new do |save|
+        save.date = Time.now
+        save.player_name = game.configuration.player_name
+        save.winner = game.won?
+        save.level = game.configuration.level
+        save.score = game.score
+      end
+    end
+
     def new_game
       print message['alerts']['new_game']
       if input_selector
