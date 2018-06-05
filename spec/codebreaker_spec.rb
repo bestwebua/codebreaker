410744f9bf6957f211b1fc55c7f9a3d7ad4a3d66
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-06-05 16:56:10 +0300

    Add Codebreaker constants tests

---
+++ spec/codebreaker_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/codebreaker'
+
+module Codebreaker
+  RSpec.describe Codebreaker do
+    specify { should be_const_defined(:VERSION) }
+    specify { should be_const_defined(:Localization) }
+    specify { should be_const_defined(:Game) }
+    specify { should be_const_defined(:GameConfiguration) }
+    specify { should be_const_defined(:Score) }
+    specify { should be_const_defined(:Console) }
+  end
+end
