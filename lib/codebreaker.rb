dd3c925168f13a89cec904b346e6b1cb0a7a5584
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-06-12 14:22:11 +0300

    Add message require

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,5 +1,6 @@
 require_relative 'codebreaker/version'
 require_relative 'codebreaker/localization'
+require_relative 'codebreaker/message'
 require_relative 'codebreaker/game'
 require_relative 'codebreaker/score'
 require_relative 'codebreaker/console'
