da449597592bd17a012240bf6793ab9503a16756
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-06-18 10:56:17 +0300

    Refactor dependencies tests

--- spec/codebreaker_spec.rb
+++ spec/codebreaker_spec.rb
@@ -4,11 +4,11 @@ module Codebreaker
   RSpec.describe Codebreaker do
     specify { should be_const_defined(:VERSION) }
     specify { should be_const_defined(:Localization) }
+    specify { should be_const_defined(:Message) }
+    specify { should be_const_defined(:Storage) }
     specify { should be_const_defined(:Game) }
     specify { should be_const_defined(:GameConfiguration) }
-    specify { should be_const_defined(:Message) }
     specify { should be_const_defined(:Score) }
-    specify { should be_const_defined(:Storage) }
     specify { should be_const_defined(:Console) }
   end
 end
