fa729dadef9183a6d9108bf8d1a17464d46d8dc6
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-06-20 15:11:28 +0300

    Add Motivation module require

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,6 +1,7 @@
 require_relative 'codebreaker/version'
 require_relative 'codebreaker/localization'
 require_relative 'codebreaker/message'
+require_relative 'codebreaker/motivation'
 require_relative 'codebreaker/storage'
 require_relative 'codebreaker/game'
 require_relative 'codebreaker/score'
